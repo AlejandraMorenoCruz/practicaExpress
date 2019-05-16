e854fc1ecc34381dd3013e66fe163eb5a398a8b5
Alejandra.Moreno <ale.mocr@gmail.com>
2019-05-16 11:59:00 -0500

    primeros cambios

--- Practice.js
+++ Practice.js
@@ -2,16 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.all('/hello', function(req, res){
-  res.send('Hello Wordl!');
-});
-
-app.all('/bye', function(req, res){
-  res.send('Bye bye!');
-});
-
+app.all('/hello', function(req, res)=>{
+){ res.send('Hello Wordl!');});
+app.all('/bye', function(req, res){ res.send('Bye bye!');});
 app.use('/assets', express.static(path.join(__dirname,'assets')));
 
-app.listen(3000,function(){
-  console.log('La app de ejemplo está escuchando en el puerto 3000');
-});
+app.get ('/nada',(req, res)=>{
+  res.status(266).send('si hay')
+})
+
+app.post('/algo',(req, res)=>{
+  res.status(404).send({foo:"bar"})
+})
+
+app.patch('/pues',req, res)=>{
+  res.status(401)
+})
+
+}
+app.listen(3000,function(){ console.log('La app de ejemplo está escuchando en el puerto 3000');});
