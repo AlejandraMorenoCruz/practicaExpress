fbbd12a2d29a9fc1b3641f9bc3cf66e056bc5a52
Alejandra.Moreno <ale.mocr@gmail.com>
2019-05-16 15:25:46 -0500

    terceros cambios, y mas...

--- Practice.js
+++ Practice.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
 const path = require('path')
+
+let libros = [];
+
+app.use (bodyParser.json())
+app.use(cookieParser())
+app.use('/assets', express.static(path.join(__dirname,'assets')));
 
 app.all('/hello', (req, res)=>{
   res.send('Hello Wordl!');
 });
 app.all('/bye', function(req, res){ res.send('Bye bye!');});
-app.use('/assets', express.static(path.join(__dirname,'assets')));
 
 app.get ('/nada',(req, res)=>{
   res.status(266).send('si hay')
@@ -29,5 +37,22 @@ app.get('/hola',(req, res)=>{
 app.get('/adios',(req, res)=>{
   res.send('nos vemos');
 })
+
+//no tengo mucha idea de que estoy haciendo, pero salió bien :)
+app.get ('/libros',(req, res)=>{
+  res.status(200).send({data:libros})
+  console.log(req.body)
+})
+
+app.post('/libros',(req, res)=>{
+  if(req.body.autor && req.body.titulo){
+    libros.push(req.body)
+    res.status(201).send('libro agregado')
+  } else{
+    res.status(400).send('los datos están incompletos')
+  }
+})
+
+
 
 app.listen(3000,function(){ console.log('La app de ejemplo está escuchando en el puerto 3000');});
