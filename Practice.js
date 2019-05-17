a1b2147864945ebb37f3bb13b7d74ba4b1102c96
Alejandra.Moreno <ale.mocr@gmail.com>
2019-05-16 23:49:33 -0500

    lo que quedó de tarea

--- Practice.js
+++ Practice.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-
-const app = express()
+const jwt = require('jsonwebtoken')
 const path = require('path')
 
+const app = express()
+
+const noDigas = 'verdeAzul'
 let libros = [];
 
 app.use (bodyParser.json())
@@ -14,7 +16,9 @@ app.use('/assets', express.static(path.join(__dirname,'assets')));
 app.all('/hello', (req, res)=>{
   res.send('Hello Wordl!');
 });
-app.all('/bye', function(req, res){ res.send('Bye bye!');});
+app.all('/bye', (req, res)=>{
+  res.send('Bye bye!');
+});
 
 app.get ('/nada',(req, res)=>{
   res.status(266).send('si hay')
@@ -42,6 +46,7 @@ app.get('/adios',(req, res)=>{
 app.get ('/libros',(req, res)=>{
   res.status(200).send({data:libros})
   console.log(req.body)
+  console.log(req.cookies)
 })
 
 app.post('/libros',(req, res)=>{
@@ -53,6 +58,42 @@ app.post('/libros',(req, res)=>{
   }
 })
 
+jwt.sign({ user: req.body.user, theme: 'green' }, noDigas, function(err, token) {
+  if(err) {
+    res.send(500).end();
+  } else {
+    res.status(200).send({token: token})
+    }
+});
 
+app.post('/auth/check',(req, res)=>{
+  console.log(req.headers)
+
+  jwt.verify(req.headers.authorization,noDigas,function(err, decoded){
+    if(err){
+      res.status(500).end()
+    }else{
+      console.lod(decoded)
+      res.status(200).end()
+    }
+  })
+})
+
+  app.use((req,res,next)=>{
+    jwt.verify(req.headers.authorization, noDigas, function(err, decoded) {
+      if(err) {
+        res.status(500).end('Terrible error')
+      } else {
+        console.log(decoded)
+        // checar ese usuario en la base datos a ver si existe
+        //poner next
+        next
+      }
+    });
+  })
+
+  app.get('/colores',(req,res)=>{
+    res.send('bienvenido a los colores')
+  })
 
 app.listen(3000,function(){ console.log('La app de ejemplo está escuchando en el puerto 3000');});
